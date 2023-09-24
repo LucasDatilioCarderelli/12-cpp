b07ed1d82cd9de2830998bd799f789b8e98f7892
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2023-09-24 10:50:03 -0300

    update: main tests

--- cpp05/ex00/sources/main.cpp
+++ cpp05/ex00/sources/main.cpp
@@ -6,27 +6,33 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 21:46:42 by ldatilio          #+#    #+#             */
-/*   Updated: 2023/09/23 13:08:25 by ldatilio         ###   ########.fr       */
+/*   Updated: 2023/09/24 10:46:48 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
+	std::cout << std::endl << "--- Testing form grade too high ---" << std::endl;
     try {
         Bureaucrat  a("Amy", 0);
     } catch (const std::exception & e) {
         std::cout << e.what() << std::endl;
     }
+
+	std::cout << std::endl << "--- Testing form grade too low ---" << std::endl;
     try {
         Bureaucrat  a("John", 151);
     } catch (const std::exception & e) {
         std::cout << e.what() << std::endl;
     }
+
+    std::cout << std::endl << "--- Testing copy constructor ---" << std::endl;
     Bureaucrat  a("Bob", 15);
     Bureaucrat  b = a;
-    std::cout << a << std::endl;
     std::cout << b << std::endl;
+    
+    std::cout << std::endl << "--- Testing increment ---" << std::endl;
     while (true) {
         try {
             ++a;
@@ -36,6 +42,8 @@ int main(void) {
         }
     }
     std::cout << a << std::endl;
+    
+    std::cout << std::endl << "--- Testing decrement ---" << std::endl;
     while (true) {
         try {
             --a;
@@ -45,5 +53,8 @@ int main(void) {
         }
     }
     std::cout << a << std::endl;
+
+    std::cout << std::endl << "--- Destructor calls ---" << std::endl;
+
     return 0;
 }
