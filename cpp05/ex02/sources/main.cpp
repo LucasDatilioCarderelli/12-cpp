c727bc1dfe2303a4c03056d76da97c2560b68975
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2023-09-24 20:05:04 -0300

    update: main tests

--- cpp05/ex02/sources/main.cpp
+++ cpp05/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 21:46:42 by ldatilio          #+#    #+#             */
-/*   Updated: 2023/09/24 18:04:46 by ldatilio         ###   ########.fr       */
+/*   Updated: 2023/09/24 20:04:43 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,55 +52,7 @@ int main(void) {
 		std::cerr << e.what() << std::endl;
 	}
 	president.signForm(pardon);
-	pardon.execute(president);		
-	
-	
-	// std::cout << std::endl << "--- Testing form grade too high ---" << std::endl;
-	// try {
-	// 	Form form("feedback", 0, 1);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-
-	// std::cout << std::endl << "--- Testing form grade too low ---" << std::endl;
-	// try {
-	// 	Form form("Feedback2", 1, 151);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-
-	// std::cout << std::endl << "--- Testing sign grade too low ---" << std::endl;
-	// Bureaucrat bob("Bob", 43);
-	// Form feedback("Feedback", 42, 42);
-	// try {
-	// 	bob.signForm(feedback);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-
-	// std::cout << std::endl << "--- Testing Bureaucrat signForm ---" << std::endl;
-	// try {
-	// 	std::cout << bob;
-	// 	std::cout << feedback;
-	// 	bob.signForm(feedback);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-
-	// std::cout << std::endl << "--- Testing signForm again ---" << std::endl;
-	// Bureaucrat amy("Amy", 21);
-	// try {
-	// 	std::cout << feedback;
-	// 	amy.signForm(feedback);
-	// 	feedback.beSigned(amy);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	pardon.execute(president);
 
 	std::cout << std::endl << "--- Destructor calls ---" << std::endl;
 
